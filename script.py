0dda2545e54684e5d106f49a51c38f679e9699d8
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2022-01-09 19:21:17 -0500

    script running success

--- script.py
+++ script.py
@@ -1,22 +1,40 @@
 import time
+import random
+from bs4 import BeautifulSoup
 from selenium import webdriver as wd
+# from selenium.webdriver.chrome.options import Options
+
+
+# from webdriver_manager.chrome import ChromeDriverManager
+
+# driver = wd.Chrome(ChromeDriverManager().install())
+
+# wd.implicitly_wait(60)
+
 
 URL="https://www.bestbuy.com/site/searchpage.jsp?id=pcat17071&qp=gpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~NVIDIA%20GeForce%20RTX%203060%5Egpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~NVIDIA%20GeForce%20RTX%203060%20Ti%5Egpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~NVIDIA%20GeForce%20RTX%203070&sp=%2Bcurrentprice%20skuidsaas&st=graphics+card"
 
-browser = wd.Chrome(URL)
-
+options = wd.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+wd = wd.Chrome(options=options)
 
 wd.get(URL)
+# wd.find_element_by_xpath()
+
 wd.set_window_size(1920,1080)
-wd.save_screenshot(screenshot.png)
+wd.save_screenshot("screenshot.png")
 
-from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(wd.page_source)
-items = soup.find("li",{"class":"sku-item"})
+soup = BeautifulSoup(wd.page_source, features="html.parser")
+lists = soup.find("ol",{"class":"sku-item-list"})
+# print(lists);
 
-print(len(items.findAll("li")));
+items = lists.findAll("li",{"class": "sku-item"})
 
+for item in items:
+    print(item.text);
+    
+print(len(items))
 
 
 
